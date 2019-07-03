e541646e044950a2ab30a5f84a99d054de5657bd
kobilee <kobiatlaslee@gmail.com>
2019-07-03 11:59:45 -0400

    add Kitten UI

--- App.js
+++ App.js
@@ -1,84 +1,13 @@
-import React from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-  Button,
-  AppRegistry,
-} from 'react-native';
-import Voice from 'react-native-voice';
-export default class VoiceNative extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      recognized: '',
-      started: '',
-      results: [],
-      test: "",
-    };
-    Voice.onSpeechStart = this.onSpeechStart.bind(this);
-        Voice.onSpeechRecognized = this.onSpeechRecognized.bind(this);
-        Voice.onSpeechResults = this.onSpeechResults.bind(this);
-  }
+import * as React from 'react';
+import { mapping, light as lightTheme } from '@eva-design/eva';
+import { ApplicationProvider, Layout } from 'react-native-ui-kitten';
 
+const App = () => (
+  <ApplicationProvider
+    mapping={mapping}
+    theme={lightTheme}>
+    <Layout style={{flex: 1}}/>
+  </ApplicationProvider>
+);
 
-componentWillUnmount() {
-    Voice.destroy().then(Voice.removeAllListeners);
-  }
-onSpeechStart(e) {
-    this.setState({
-      started: '√',
-    });
-  };
-onSpeechRecognized(e) {
-    this.setState({
-      recognized: '√',
-    });
-  };
-onSpeechResults(e) {
-    this.setState({
-      results: e.value,
-    });
-  }
-async _startRecognition(e) {
-    this.setState({
-      recognized: '',
-      started: '',
-      results: [],
-    });
-    try {
-      await Voice.start('en-US');
-    } catch (e) {
-      console.error(e);
-    }
-  }
-render () {
-  if (this.state.results[0] == "done"){
-      setTimeout(() => {
-        this.setState({test: "test"})
-      }, 3000);
-   }
-    console.log(this.state.results[0])
-    return (
-      <View>
-        <Text style={styles.transcript}>
-            Transcript
-        </Text>
-        <Text style={styles.transcript}> {this.state.results[0]} </Text>
-        <Text style={styles.transcript}> {this.state.test} </Text>
-        <Button style={styles.transcript}
-        onPress={this._startRecognition.bind(this)}
-        title="Start"></Button>
-      </View>
-    );
-      }
-}
-const styles = StyleSheet.create({
-  transcript: {
-    textAlign: 'center',
-    color: '#B0171F',
-    marginBottom: 1,
-    top: '400%',
-  },
-});
-AppRegistry.registerComponent('VoiceNative', () => VoiceNative);
+export default App;
